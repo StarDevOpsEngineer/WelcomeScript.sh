e0346fe39bb5c2608ab82f532a24c2781216f2fa
Esther A-Nalley <99775062+StarDevOpsEngineer@users.noreply.github.com>
2022-02-26 18:28:34 -0600

    Update WelcomeScript.sh

--- WelcomeScript.sh
+++ WelcomeScript.sh
@@ -3,15 +3,15 @@
 echo "Hello! Welcome to Alliant Credit Union!"
 echo "Are you an Alliant Member?"
 read answer
-if [$Answer == yes]
-then 
-	echo "Please Enter Your Username:"
-	read username
-	echo "Please Enter Your Password:"
-	read -s passwd
-	echo "Welcome to Alliant Credit Union $Username!"
-	echo "What would you like to do?"
+if [ $answer = yes ]
+then
+        echo "Please Enter Your Username:"
+        read Username
+        echo "Please Enter Your Password:"
+        read -s Passwd
+        echo "Welcome to Alliant Credit Union $Username!"
+        echo "What would you like to do?"
 else
-	echo "You must be a Member to use this section."
-	echo "Please go to https://www.alliantcreditunion.org to Create an Account"
+        echo "You must be a member to this section."
+        echo "Please go to https://www.alliantcreditunion.org"
 fi
